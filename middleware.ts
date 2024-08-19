732f568f40a62b5d666fa496d06cb816b0b9748f
codebreaker02 <jordan.vitou@epitech.eu>
2024-08-19 22:07:07 +0100

    fix: mobile issue sheet

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,40 @@
 import { NextResponse } from "next/server";
 import { auth } from "@/auth";
 
-import {
-  apiAuthPrefix,
-  authRoutes,
-  defaultLoginRedirect,
-  publicRoutes,
-} from "@/routes";
+import { apiAuthPrefix, authRoutes, publicRoutes } from "@/routes";
+import { getDefaultPageForRole, hasAccess } from "@/lib/utils";
 
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
+  // @ts-ignore
+  const userRole = req.auth?.user?.role;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) return NextResponse.next();
+  if (isApiAuthRoute) {
+    return NextResponse.next();
+  }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return NextResponse.redirect(new URL(defaultLoginRedirect, nextUrl));
+      const defaultPage = getDefaultPageForRole(userRole);
+      return NextResponse.redirect(new URL(defaultPage, nextUrl));
     }
     return NextResponse.next();
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return NextResponse.redirect(new URL("/login", nextUrl));
+    const loginUrl = new URL("/login", nextUrl);
+    loginUrl.searchParams.set("callbackUrl", nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if (isLoggedIn && !isPublicRoute && !hasAccess(nextUrl.pathname, userRole)) {
+    const defaultPage = getDefaultPageForRole(userRole);
+    return NextResponse.redirect(new URL(defaultPage, nextUrl));
   }
 
   return NextResponse.next();
