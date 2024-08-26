83f9e918b7ca24ec92d7a420d90cb7f55d680d54
codebreaker02 <jordan.vitou@epitech.eu>
2024-08-26 02:37:53 +0100

    fix: remove link from user nave

--- lib/schemas/players/players.ts
+++ lib/schemas/players/players.ts
@@ -3,18 +3,18 @@ import { z } from "zod";
 export const players = z.object({
   firstName: z.string().min(1, { message: "First Name is required" }),
   lastName: z.string().min(1, { message: "Last Name is required" }),
-  playerImage: z.union([z.string(), z.instanceof(File)]).optional(),
+  playerImage: z
+    .union([z.string(), z.instanceof(File), z.undefined()])
+    .optional(),
   dorseyNumber: z
     .number({ message: "Dorsey Number is required and must be a number" })
     .nonnegative("Dorsey Number must be a non-negative number")
     .int("Dorsey Number must be an integer"),
   college: z.string().min(1, { message: "College is required" }),
   nationality: z.string().min(1, { message: "Nationality is required" }),
-  playerEmail: z.string().email({ message: "Invalid email address" }),
-  countryCode: z.string().min(1, "Country code is required"),
-  phoneNumber: z
-    .number({ message: "Phone Number is required and must be a number" })
-    .positive("Height must be a positive number"),
+  playerEmail: z.string().email().optional(),
+  countryCode: z.string().optional(),
+  phoneNumber: z.number().optional(),
   position: z.string().min(1, { message: "Position is required" }),
   height: z
     .number({ message: "Height is required and must be a number" })
@@ -45,7 +45,9 @@ export const players = z.object({
 export const playerEditSchema = z.object({
   firstName: z.string().optional(),
   lastName: z.string().optional(),
-  playerImage: z.union([z.string(), z.instanceof(File)]).optional(),
+  playerImage: z
+    .union([z.string(), z.instanceof(File), z.undefined()])
+    .optional(),
   dorseyNumber: z
     .number({ message: "Dorsey Number is required and must be a number" })
     .nonnegative("Dorsey Number must be a non-negative number")
